b28a73a2694d65216b36a1723198a9c0d2f89871
RSamaium <sam@newbot.io>
2025-11-28 16:51:01 +0100

    hp-bar

---
+++ packages/client/src/components/prebuilt/index.ts
@@ -0,0 +1,21 @@
+/**
+ * Prebuilt sprite components for common UI elements
+ * 
+ * This module exports ready-to-use components that can be attached
+ * to sprites using componentsInFront or componentsBehind configuration.
+ * 
+ * @example
+ * ```ts
+ * import { HpBar } from '@rpgjs/client/components/prebuilt';
+ * 
+ * export default defineModule<RpgClient>({
+ *   sprite: {
+ *     componentsInFront: [HpBar]
+ *   }
+ * })
+ * ```
+ */
+
+export { default as HpBar } from './hp-bar.ce';
+
+
